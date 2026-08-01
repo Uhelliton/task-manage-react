2a429570d136cadc8108c57f441db038280206b5
Uhelliton Andrade <uhelliton@uol.com.br>
2026-08-01 15:09:29 -0300

    Refactor `CreateTask` component to support task creation via props and integrate with `App` component.

--- src/App.tsx
+++ src/App.tsx
@@ -11,29 +11,35 @@ function App() {
     { id: 4, title: 'Task 4', description: 'Description 4', completed: true },
     { id: 5, title: 'Task 5', description: 'Description 5', completed: false },
   ]);
-
-  const options = [
-    { label: 'Create a new project', value: 'new' },
-    { label: 'Open an existing project', value: 'open' },
-    { label: 'Exit', value: 'exit' },
-    { label: 'About', value: 'about' },
-  ];
   
   const handleTaskComplete = (task) => {
     setTasks(tasks.map(t => t.id === task.id ? {...t, completed: !t.completed} : t));
   }
   
-  const onTaskRemove = (task) => {
+  const removeTask = (task) => {
     setTasks(tasks.filter(t => t.id !== task.id));
+  }
+  
+  const createTask = (title: string) => {
+    console.log(title)
+    const newTask = {
+      id: Date.now(),
+      title: title,
+      description: 'Description',
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
   }
   
   return (
     <div className="max-w-lg mx-auto my-10 bg-white p-8 rounded-xl shadow shadow-slate-300">
-      <CreateTask/>
+      <CreateTask
+        createTask={createTask}
+      />
       <Task
         tasks={tasks}
         onTaskClick={handleTaskComplete}
-        onTaskRemove={onTaskRemove}
+        onTaskRemove={removeTask}
       />
       <p className="text-xs text-slate-500 text-center">Last updated 12 minutes ago</p>
     </div>
--- src/components/CreateTask.tsx
+++ src/components/CreateTask.tsx
@@ -1,28 +1,35 @@
-function CreateTask() {
+import { Plus } from 'lucide-react';
+import { JSX, useState } from "react";
+
+type CreateTaskProps = {
+  createTask: (title: string) => void;
+};
+
+function CreateTask({ createTask }: CreateTaskProps) : JSX.Element {
+  const [title, setTitle] = useState('');
+  
+  const handleSubmit = () => {
+    console.log('chegou aqui')
+    createTask(title)
+    setTitle('')
+  }
+  
     return (
-       <form>
+       <form className="mb-4">
          <div className="flex flex-row justify-between items-center">
-           <div>
-             <h1 className="text-3xl font-medium">Tasks list</h1>
-           </div>
            <div className="inline-flex space-x-2 items-center">
-             <a href="#"
-                className="p-2 border border-slate-200 rounded-md inline-flex space-x-1 items-center text-indigo-200 hover:text-white bg-indigo-600 hover:bg-indigo-500">
-               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" className="w-4 h-4">
-                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
-               </svg>
-               <span className="text-sm font-medium hidden md:block">Urgent</span>
-             </a>
-             <a href="#"
-                className="p-2 border border-slate-200 rounded-md inline-flex space-x-1 items-center hover:bg-slate-200">
-               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" className="w-4 h-4">
-                 <path stroke-linecap="round" stroke-linejoin="round"
-                       d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"/>
-               </svg>
-               <span className="text-sm hidden md:block">Latest</span>
-             </a>
+             <input className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+                    placeholder="Add Task"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+             />
+             <button
+               type={'button'}
+               onClick={handleSubmit}
+                className="p-2.5 border border-slate-200 rounded-md inline-flex space-x-1 items-center text-indigo-200 hover:text-white bg-indigo-600 hover:bg-indigo-500">
+               <Plus className="w-4 h-4"/>
+               <span className="text-sm font-medium hidden md:block">ADD</span>
+             </button>
            </div>
          </div>
        </form>
